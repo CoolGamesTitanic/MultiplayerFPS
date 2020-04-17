f9b713be80ebd0411815e1e988a480476c169380
The-Bobby-tarantinos <ethanbwalz@icloud.com>
2020-04-16 17:28:44 -0700

    Initial commit

---
+++ Source/MultiplayerFPS/MultiplayerFPSGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiplayerFPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERFPS_API AMultiplayerFPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
